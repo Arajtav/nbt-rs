08c4a28943a842e34f062b544ea86de9ee64263d
Aräjtav <arajtav@arajtav.com>
2025-08-05 22:25:06 +0200

    fix: return the Vec on try_from failure on Array, store data as boxed slice in Array

--- src/tags/array.rs
+++ src/tags/array.rs
@@ -4,29 +4,33 @@ use crate::tags::ValidationError;
 
 #[derive(Debug, PartialEq)]
 pub struct Array<T> {
-    data: Vec<T>,
+    data: Box<[T]>,
 }
 
 impl<T> Array<T> {
     pub(crate) fn new_unchecked(data: Vec<T>) -> Self {
-        Self { data }
+        Self {
+            data: data.into_boxed_slice(),
+        }
     }
 }
 
 impl<T> TryFrom<Vec<T>> for Array<T> {
-    type Error = ValidationError;
+    type Error = (ValidationError, Vec<T>);
     fn try_from(vec: Vec<T>) -> Result<Self, Self::Error> {
         if vec.len() > i32::MAX as usize {
-            Err(ValidationError::ArrayTooLong(vec.len()))
+            Err((ValidationError::ArrayTooLong(vec.len()), vec))
         } else {
-            Ok(Self { data: vec })
+            Ok(Self {
+                data: vec.into_boxed_slice(),
+            })
         }
     }
 }
 
 impl<T> From<Array<T>> for Vec<T> {
     fn from(array: Array<T>) -> Self {
-        array.data
+        array.data.into_vec()
     }
 }
 
