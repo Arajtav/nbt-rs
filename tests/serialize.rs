30e9aa455511bab0114e7f2874e045a2c1c70c64
Aräjtav <arajtav@arajtav.com>
2025-08-09 22:52:46 +0200

    fix: add missing check in serialize test, fix typo

--- tests/serialize.rs
+++ tests/serialize.rs
@@ -4,10 +4,13 @@ use nbt_rs::{parser::parse_nbt, serializer::serialize_nbt};
 fn test_reparse_level_dat() {
     // i guess this test depends on whether the parsing works correctly, which is not optimal. it is
     // however the best i can do, as the Compound/HashMap doesn't care about the order of tags,
-    // making comparision between hand written binary, and serialized object impossible.
-    let (name, parsed) = parse_nbt(include_bytes!("data/level_uncompressed.dat")).unwrap();
+    // making comparison between hand written binary, and serialized object impossible.
+    let data = include_bytes!("data/level_uncompressed.dat");
+    let (name, parsed) = parse_nbt(data).unwrap();
     let serialized = serialize_nbt(&name, &parsed);
     let (new_name, new_parsed) = parse_nbt(&serialized).unwrap();
+    // the amount of data should stay the same though.
+    assert_eq!(data.len(), serialized.len());
     assert_eq!(name, new_name);
     assert_eq!(parsed, new_parsed);
 }
