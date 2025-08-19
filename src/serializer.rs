dea95b75904c8d12c6237762b2b3b2b979dcdfaf
Aräjtav <arajtav@arajtav.com>
2025-08-19 22:19:07 +0200

    perf: preallocate 4KiB in serialize_nbt

--- src/serializer.rs
+++ src/serializer.rs
@@ -11,11 +11,12 @@ use crate::{
 /// # Parameters
 /// - `name`: The name of the root tag. Typically an empty string.
 /// - `data`: The compound with data to serialize.
-pub fn serialize_nbt(name: &NbtString, data: &NbtCompound) -> Box<[u8]> {
-    // would have just used serialize_named_tag, however i would need to clone for Tag::Compound()
-    let mut buffer = Vec::new();
+pub fn serialize_nbt(name: &NbtString, data: &NbtCompound) -> Vec<u8> {
+    // 4KiB is small enough to not matter compared how much
+    // does it speed thing up for an average nbt file
+    let mut buffer = Vec::with_capacity(4096);
     buffer.push(NbtTagId::Compound as u8);
     name.serialize_nbt_payload(&mut buffer);
     data.serialize_nbt_payload(&mut buffer);
-    buffer.into_boxed_slice()
+    buffer
 }
